68d9bdd0378e6256be92d5d13f0649c7defd7088
hassannazir <hassan.nazir1998@gmail.com>
2022-06-09 13:06:51 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "h8k-components";
+
+import { Movieform, Movieslist, Search } from "./components";
+import { useState } from "react";
+const title = "Favorite Movie Directory";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h8k-navbar header={title} />
+      <div className="layout-row justify-content-center mt-100">
+        <div className="w-30 mr-75">
+          <Movieform
+            movieList={movieList}
+            setMovieList={setMovieList}
+            filterList={filterList}
+            setFilterList={setFilterList}
+          />
+        </div>
+        <div className="layout-column w-30">
+          <Search
+            movieList={movieList}
+            setMovieList={setMovieList}
+            filterList={filterList}
+            setSearchQuery={setSearchQuery}
+          />
+
+          <Movieslist movieList={movieList} setMovieList={setMovieList} />
+          {searchQuery && (
+            <div data-testid="noResult">
+              <h3 className="text-center">No Results Found</h3>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
